4e7eb2f20b2b43eb208745c823d7ae954d3249e7
Or Bitton <orbitzx25@gmail.com>
2025-12-28 17:20:40 +0200

    connecting to MongoDB atlas

--- services/users-service/src/app.js
+++ services/users-service/src/app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
+const connectDB = require('./config/db');
+
 
 const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
+
+connectDB();
+
 
 app.listen(PORT, () => {
     console.log(`Users service running on port ${PORT}`);
