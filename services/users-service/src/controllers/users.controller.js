3d8cfae0d94305f16403d4d1cf727cfcc10b13fc
Or Bitton <orbitzx25@gmail.com>
2026-01-06 22:47:21 +0200

    creating user & report controllers

---
+++ services/users-service/src/controllers/users.controller.js
@@ -0,0 +1,49 @@
+const User = require('../models/user.model');
+
+const addUser = async (userData) => {
+    const { id, first_name, last_name, birthday } = userData; //put the data into variables
+
+    if (!id || !first_name || !last_name || !birthday) {
+        throw new Error('Missing required user fields');
+    }
+
+    const existingUser = await User.findOne({ id }); //find if this id exist
+
+    if (existingUser) {
+        throw new Error('User with this id already exists');
+    }
+
+    const user = new User({
+        id,
+        first_name,
+        last_name,
+        birthday
+    });
+
+    await user.save(); //saving the user in DB
+
+    return user; //returning the object
+};
+
+const getUserDetails = async (userId) => {
+
+    if (!userId) {
+        throw new Error('User Id is required');
+    }
+
+    const user = await User.findOne ({id :userId});
+
+    const total = 0;
+
+    return {
+        id: user.id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        total
+    };
+
+}
+
+module.exports = {
+    addUser , getUserDetails
+};
