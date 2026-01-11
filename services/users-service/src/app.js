28cfe05b78792c60651fbd92ecb9bdd3a57327ec
Or Bitton <orbitzx25@gmail.com>
2026-01-12 01:35:27 +0200

    implemented logger

--- services/users-service/src/app.js
+++ services/users-service/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const usersRoutes = require('./routes/users.routes');
 const reportsRoutes = require('./routes/reports.routes');
+const requestLogger = require('./middleware/logger.middleware');
+
 
 const app = express();
 
@@ -10,11 +12,9 @@ const connectDB = require('./config/db');
 const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
+app.use(requestLogger);
 
-app.use((req, res, next) => { // validation
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
+
 
 app.use('/api', usersRoutes);
 app.use('/api', reportsRoutes);
