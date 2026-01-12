ac02d2ab06952b2f1b6dcdd27617d665f44971d6
yahav <reczza161@gmail.com>
2026-01-12 16:28:55 +0200

    step 5 6

--- services/costs-service/src/app.js
+++ services/costs-service/src/app.js
@@ -2,10 +2,34 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
+const connectDB = require('./config/db');
+
 
 const PORT = process.env.PORT || 3001;
 
+
+const requestLogger = require('./middlewares/requestLogger');
+//const costsRoutes = require('./routes/costs.routes');
+
+app.use(requestLogger);
 app.use(express.json());
+
+
+
+
+
+//use('/api', costsRoutes);
+
+connectDB();
+
+
+
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'costs test ok' });
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Costs service running on port ${PORT}`);
--- services/users-service/src/app.js
+++ services/users-service/src/app.js
@@ -5,9 +5,17 @@ require('dotenv').config();
 const connectDB = require('./config/db');
 
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
+
+
+const requestLogger = require('./middlewares/requestLogger');
+//const usersRoutes = require('./routes/users.routes');
 
 app.use(express.json());
+
+app.use(requestLogger);
+
+//use('/api', userRoutes);
 
 connectDB();
 
